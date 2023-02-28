13a290de4c046a01bd0a4a003bfbda123c5dce0d
wissanu17 <119285633+wissanu17@users.noreply.github.com>
2023-02-28 19:25:29 +0700

    Edit method

---
+++ rayayoi/wissanu/lab6/AthleteV2.java
@@ -0,0 +1,22 @@
+ /**
+ * This class is name AthleteV2 that extend Athlete from lab5.
+ *
+ * Authur: Wissanu Rayayoi
+ * ID : 653040460-3
+ * Sec.1
+ * Date : Febuary 3, 2022
+ * 
+ */
+package rayayoi.wissanu.lab6;
+
+public class AthleteV2 extends rayayoi.wissanu.lab5.Athlete {
+
+    public AthleteV2(String name, double wieght, double height, Gender gender, String nationality, String birthdate) {
+        super(name, wieght, height, gender, nationality, birthdate);
+    }
+
+    void practice() {
+        System.out.println(super.name + "run for 10 km");
+    }
+    
+}
