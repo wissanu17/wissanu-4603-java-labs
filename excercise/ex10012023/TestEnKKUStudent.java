ee9fc05d9a210cf4bf86f5c8b0dae6a4a1be21f1
wissanu17 <119285633+wissanu17@users.noreply.github.com>
2023-03-17 22:23:26 +0700

    deee

--- excercise/ex10012023/TestEnKKUStudent.java
+++
@@ -1,8 +0,0 @@
-package wissanu.ex10012023;
-public class TestEnKKUStudent {
-    public static void main(String[] args) {
-        EnKKUStudent manee = new EnKKUStudent("Manee");
-        manee.setGearNumber(30);
-        System.out.print(manee);
-    }
-}
